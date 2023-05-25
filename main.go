81c5ca5f75e7ce0fb602569c782da75d3ad83567
simonaditia <smnsitumeang@gmail.com>
2023-05-25 17:40:28 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"log"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,30 +13,7 @@ func main() {
 	router.GET("/ilt6", ilt6)
 	router.GET("/halo", halo)
 
-	// router.Run(":8080")
-	
-	// Jalankan server HTTP di latar belakang
-	go func() {
-		if err := http.ListenAndServe(":8080", router); err != nil {
-			log.Fatal("Error starting HTTP server:", err)
-		}
-	}()
-
-	// Konfigurasi server HTTPS
-	certFile := "cert.pem"
-	keyFile := "key.pem"
-
-	// Jalankan server HTTPS di latar belakang
-	go func() {
-		if err := http.ListenAndServeTLS(":8443", certFile, keyFile, router); err != nil {
-			log.Fatal("Error starting HTTPS server:", err)
-		}
-	}()
-
-	// Jalankan reverse proxy untuk mengarahkan lalu lintas HTTP ke HTTPS
-	if err := http.ListenAndServe(":80", http.HandlerFunc(redirectHTTP)); err != nil {
-		log.Fatal("Error starting reverse proxy:", err)
-	}
+	router.Run(":8080")
 }
 
 func ping(c *gin.Context) {
@@ -72,10 +48,7 @@ func halo(c *gin.Context) {
 	)
 }
 
-func redirectHTTP(w http.ResponseWriter, req *http.Request) {
-	target := "https://" + req.Host + req.URL.Path
-	http.Redirect(w, req, target, http.StatusMovedPermanently)
-}
+
 
 
 /*
