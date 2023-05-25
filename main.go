e213ce312b8ca6660f6c3b4ff8a7f5b573657e2d
simonaditia <smnsitumeang@gmail.com>
2023-05-25 16:41:12 +0700

    Update main.go & cloudbuild.yaml

--- main.go
+++ main.go
@@ -2,6 +2,60 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", ping)
+	router.GET("/hello", hello)
+	router.GET("/ilt6", ilt6)
+	router.GET("/halo", halo)
+
+	router.Run(":8080")
+}
+
+func ping(c *gin.Context) {
+	c.Data(
+		http.StatusOK,
+		"application/json",
+		[]byte(`{"message": "pong"}`),
+	)
+}
+
+func hello(c *gin.Context) {
+	c.Data(
+		http.StatusOK,
+		"application/json",
+		[]byte(`{"message": "hello ilt 6!"}`),
+	)
+}
+
+func ilt6(c *gin.Context) {
+	c.Data(
+		http.StatusOK,
+		"application/json",
+		[]byte(`{"message": "this is ilt6!"}`),
+	)
+}
+
+func halo(c *gin.Context) {
+	c.Data(
+		http.StatusOK,
+		"application/json",
+		[]byte(`{"message": "halo halo!"}`),
+	)
+}
+
+
+
+
+/*
+package main
+
+import (
+	"net/http"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -76,3 +130,4 @@ func redirectHTTP(w http.ResponseWriter, req *http.Request) {
 	target := "https://" + req.Host + req.URL.Path
 	http.Redirect(w, req, target, http.StatusMovedPermanently)
 }
+*/
